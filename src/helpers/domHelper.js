c38128114de6178363b9307752ff44a79ec0d9d5
Oscar Siregar <116549248+oscarsiregar@users.noreply.github.com>
2026-05-30 23:22:23 +0700

    Update domHelper.js

--- src/helpers/domHelper.js
+++ src/helpers/domHelper.js
@@ -1,10 +1,8 @@
 export function createElement({ tagName, className = "", attributes = {} }) {
   const element = document.createElement(tagName);
-
   if (className) {
     element.classList.add(...className.split(" ").filter(Boolean));
   }
-
   Object.keys(attributes).forEach((key) => {
     element.setAttribute(key, attributes[key]);
   });
